9dc21f8d8148beeb4f92d05a4417b81671aba17a
Kero3333 <kevin.r@institutsolacroup.com>
2021-10-10 19:04:39 +0200

    first commit

---
+++ Pythagore/pythagore.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    //on récupère la longueur de AB en tant que float
+    float longAB;
+    printf("Quelle est la longueur de AB (en cm)? ");
+    scanf("%f", &longAB);
+
+    //on récupère la longueur de BC en tant que float
+    float longBC;
+    printf("Quelle est la longueur de BC (en cm)? ");
+    scanf("%f", &longBC);
+
+    float longAC = sqrt((longAB*longAB)+(longBC*longBC)); //on calcule la longueur de AC en usant du théorème de Pythagore
+    printf("la longueur de AC est de %.2f cm\n", longAC); //on affiche au centième près la longueur de AC
+}
